d412c5b662a4bc062312ad6b54ca664336d34a94
majaspar <130063598+majaspar@users.noreply.github.com>
2023-11-02 16:46:01 +0000

    update admin dashboard

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,4 +1,28 @@
 
+$(function () {
+    $('#to-do-list ul').sortable({
+        items: "li:not('.day-of-the-week, .add-item')",
+        connectWith: "ul",
+        dropOnEmpty: true,
+        placeholder: "emptySpace"
+    });
+
+    $('.add-item input').keydown(function (e) {
+        if (e.keyCode == 13) {
+            var item = $(this).val();
+
+            $(this).parent().parent().append('<li>' + item + '</li');
+            $(this).val('');
+        }
+    })
+
+    $('#delete-box').droppable({
+        drop: function (event, ui) {
+            ui.draggable.remove();
+        }
+    })
+})
+
 
 const shareLink = encodeURI(window.location.href);
 const msg = encodeURIComponent('Hey, I found this article');
